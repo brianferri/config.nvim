a428d1cfdd9f7d81c8747da2b9e7b7b9a160c213
Brian Ferri <me@brianferri.dev>
2025-06-14 16:37:36 +0200

    feat: selection movement, proper command execution

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -1,27 +1,44 @@
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local map = function(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+end
 
--- All Modes
-A = { 'i', 'n', 'v' }
+vim.o.keymodel = "startsel,stopsel"
+local all_modes = { 'i', 'n', 'v' }
 
-map(A, '<C-p>', "<ESC>:Telescope find_files<CR>", opts)
-map(A, '<C-S-p>', "<ESC>:Telescope commands<CR>", opts)
-map(A, '<C-b>', "<ESC>:NvimTreeToggle<CR>", opts)
-map(A, '<C-g>', "<ESC>:FloatermNew lazygit<CR>", opts)
+-- Core Telescope and File Navigation
+map(all_modes, '<C-p>', "<cmd>Telescope find_files<CR>", "Telescope: Find Files (file palette)")
+map(all_modes, '<M-p>', "<cmd>Telescope commands<CR>", "Telescope: Commands (command palette)")
+map(all_modes, '<C-b>', "<cmd>NvimTreeToggle<CR>", "Toggle File Tree")
+map(all_modes, '<C-g>', "<cmd>FloatermNew lazygit<CR>", "Open LazyGit")
 
-map(A, '<C-q>', "<ESC>:qa<CR>", opts)
-map(A, '<C-w>', "<ESC>:q<CR>", opts)
-map(A, '<C-s>', "<ESC>:w<CR>i<Right>", opts)
-map(A, '<C-f>', "<ESC>/", opts)
+-- Quit, Write, Search
+map(all_modes, '<C-q>', "<cmd>qa<CR>", "Quit All")
+map(all_modes, '<C-w>', "<cmd>q<CR>", "Close Window")
+map(all_modes, '<C-s>', "<cmd>w<CR>", "Write File")
+map(all_modes, '<C-f>', "<ESC>/", "Search")
 
-map(A, '<C-a>', "<ESC>ggVG", opts)
-map(A, '<M-Up>', '<ESC>ddkP', opts)
-map(A, '<M-Down>', '<ESC>ddjP', opts)
+-- Select All, Move Lines
+map(all_modes, '<C-a>', "<ESC>ggVG", "Select All")
+map(all_modes, '<M-Up>', '<cmd>m .-2<CR>==', "Move Line Up")
+map(all_modes, '<M-Down>', '<cmd>m .+1<CR>==', "Move Line Down")
 
-map(A, "<C-k>", function() vim.lsp.buf.hover { border = 'rounded' } end, opts)
-map(A, "<C-LeftMouse>", vim.lsp.buf.implementation, opts)
-map(A, "<C-r>", vim.lsp.buf.rename, opts)
-map(A, "<C-.>", vim.lsp.buf.code_action, opts)
--- map(A, "gd", vim.lsp.buf.definition, opts)
--- map(A, "<C-S-r>", vim.lsp.buf.references, opts)
-map(A, "<C-S-i>", function() vim.lsp.buf.format { async = true } end, opts)
+map('v', '<M-Up>', ":m '<-2<CR>gv=gv", "Move Selection Up")
+map('v', '<M-Down>', ":m '>+1<CR>gv=gv", "Move Selection Down")
+map('v', '<Tab>', ">gv", "Indent Selection")
+map('v', '<S-Tab>', "<gv", "Unindent Selection")
+
+-- Copy/Cut/Paste/Undo
+map(all_modes, '<C-v>', '<Esc>pi', "Paste")
+map(all_modes, '<C-z>', '<Esc>ui', "Undo")
+
+map('v', '<C-c>', 'y<Esc>i', "Copy")
+map('v', '<C-x>', 'd<Esc>i', "Cut")
+
+-- LSP Bindings
+map(all_modes, '<C-k>', function() vim.lsp.buf.hover { border = 'rounded' } end, "LSP Hover")
+map(all_modes, '<C-LeftMouse>', vim.lsp.buf.implementation, "LSP Implementation")
+map(all_modes, '<C-r>', vim.lsp.buf.rename, "LSP Rename")
+map(all_modes, '<C-.>', vim.lsp.buf.code_action, "LSP Code Action")
+-- map(all_modes, 'gd', vim.lsp.buf.definition, "LSP Goto Definition")
+-- map(all_modes, '<C-S-r>', vim.lsp.buf.references, "LSP References")
+map(all_modes, '<C-S-i>', function() vim.lsp.buf.format { async = true } end, "LSP Format")
