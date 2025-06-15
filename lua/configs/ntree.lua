350a899915a8203e26e2b68a7d0e94430b0a9938
Brian Ferri <me@brianferri.dev>
2025-06-15 20:33:48 +0200

    feat: follow open buffer

--- lua/configs/ntree.lua
+++ lua/configs/ntree.lua
@@ -2,6 +2,9 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
+    update_focused_file = {
+        enable = true
+    },
     filters = {
         dotfiles = false,
     },
