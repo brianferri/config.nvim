f3cf53c37fb77bffe14c577f6300cffd0cd4ef18
Brian Ferri <me@brianferri.dev>
2025-06-15 18:44:11 +0200

    chore: lint, reorder keybinds

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -6,10 +6,11 @@ vim.o.keymodel = "startsel,stopsel"
 local all_modes = { 'i', 'n', 'v' }
 
 -- Core Telescope and File Navigation
-map(all_modes, '<C-p>', "<cmd>Telescope find_files<CR>", "Telescope: Find Files (file palette)")
-map(all_modes, '<M-p>', "<cmd>Telescope commands<CR>", "Telescope: Commands (command palette)")
+map(all_modes, '<M-p>', "<cmd>Telescope find_files<CR>", "Telescope: Find Files (file palette)")
+map(all_modes, '<M-S-p>', "<cmd>Telescope commands<CR>", "Telescope: Commands (command palette)")
 map(all_modes, '<C-b>', "<cmd>NvimTreeToggle<CR>", "Toggle File Tree")
 map(all_modes, '<C-g>', "<cmd>FloatermNew lazygit<CR>", "Open LazyGit")
+map(all_modes, '<C-,>', "<cmd>FloatermNew nvim ~/.config/nvim<CR>", "Open Nvim Config")
 
 -- Quit, Write, Search
 map(all_modes, '<C-q>', "<cmd>qa<CR>", "Quit All")
@@ -17,20 +18,21 @@ map(all_modes, '<C-w>', "<cmd>q<CR>", "Close Window")
 map(all_modes, '<C-s>', "<cmd>w<CR>", "Write File")
 map(all_modes, '<C-f>', "<ESC>/", "Search")
 
--- Select All, Move Lines, Word Manipulation
+-- Select All, Move Lines, Selection Manipulation
+-- TODO add comment on selection/line
 map(all_modes, '<C-a>', "<ESC>ggVG", "Select All")
-map(all_modes, '<C-d>', '<ESC><Plug>(VM-Find-Under)', "VisualMulti: Select Next Word Down")
 
-map({'i', 'n'}, '<C-M-Up>', '<ESC>yyP', "Duplicate Line Up")
-map({'i', 'n'}, '<C-M-Down>', '<ESC>yyp', "Duplicate Line Down")
-map('v', '<C-M-Up>', ":t '<-1<CR>gv=gv", "Duplicate Selection Up")
-map('v', '<C-M-Down>', ":t '>+0<CR>gv=gv", "Duplicate Selection Down")
+map({ 'i', 'n' }, '<M-C-S-Up>', '<ESC>yyP', "Duplicate Line Up")
+map({ 'i', 'n' }, '<M-C-S-Down>', '<ESC>yyp', "Duplicate Line Down")
+map('v', '<M-C-S-Up>', ":t '<-1<CR>gv=gv", "Duplicate Selection Up")
+map('v', '<M-C-S-Down>', ":t '>+0<CR>gv=gv", "Duplicate Selection Down")
 
-map({'i', 'n'}, '<M-Up>', '<cmd>m .-2<CR>', "Move Line Up")
-map({'i', 'n'}, '<M-Down>', '<cmd>m .+1<CR>', "Move Line Down")
+map({ 'i', 'n' }, '<M-Up>', '<cmd>m .-2<CR>', "Move Line Up")
+map({ 'i', 'n' }, '<M-Down>', '<cmd>m .+1<CR>', "Move Line Down")
 map('v', '<M-Up>', ":m '<-2<CR>gv=gv", "Move Selection Up")
 map('v', '<M-Down>', ":m '>+1<CR>gv=gv", "Move Selection Down")
 
+map(all_modes, '<M-BS>', "<ESC>daw", "Delete A Word")
 map('v', '<Tab>', ">gv", "Indent Selection")
 map('v', '<S-Tab>', "<gv", "Unindent Selection")
 map('v', '<BS>', '"_d', "Delete Selection")
@@ -39,7 +41,7 @@ map('v', '<BS>', '"_d', "Delete Selection")
 map(all_modes, '<C-v>', '<ESC>pi', "Paste")
 map(all_modes, '<C-z>', '<ESC>ui', "Undo")
 
-map({'i', 'n'}, '<C-x>', '<ESC>dd<ESC>i', "Cut")
+map({ 'i', 'n' }, '<C-x>', '<ESC>dd<ESC>i', "Cut")
 map('v', '<C-c>', '"+y<ESC>i', "Copy")
 map('v', '<C-x>', 'd<ESC>i', "Cut")
 
@@ -49,3 +51,7 @@ map(all_modes, '<C-LeftMouse>', vim.lsp.buf.implementation, "LSP Implementation"
 map(all_modes, '<C-r>', vim.lsp.buf.rename, "LSP Rename")
 map(all_modes, '<C-.>', vim.lsp.buf.code_action, "LSP Code Action")
 map(all_modes, '<C-S-i>', function() vim.lsp.buf.format { async = true } end, "LSP Format")
+
+-- Visual Multi
+map(all_modes, '<C-d>', '<ESC><Plug>(VM-Find-Under)', "VM: Select Next Word Down")
+map(all_modes, '<C-S-d>', '<ESC><Plug>(VM-Select-All)', "VM: Select All Occurrences")
