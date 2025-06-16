2afb5b626a038cd8ebeeac1f71a772de4c2256a3
Brian Ferri <me@brianferri.dev>
2025-06-16 12:33:25 +0200

    feat: Workspace search

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -18,7 +18,8 @@ map(all_modes, '<M-S-[>', "<CMD>BufferLineCyclePrev<CR>", "Cycle Previous Buffer
 map(all_modes, '<C-q>', "<CMD>qa<CR>", "Quit All")
 map(all_modes, '<C-w>', "<CMD>q<CR>", "Close Window")
 map(all_modes, '<C-s>', "<CMD>w<CR>", "Write File")
-map(all_modes, '<C-f>', "<ESC>/", "Search")
+map(all_modes, '<C-f>', "<ESC>/", "Search In File")
+map(all_modes, '<C-S-f>', "<CMD>Telescope live_grep<CR>", "Search All Files")
 
 -- Select All, Move Lines, Selection Manipulation
 -- TODO add comment on selection/line
