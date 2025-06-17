d5e7d5dba1a63ecbdee9da4f8c208ecc6c294e40
Brian Ferri <me@brianferri.dev>
2025-06-17 12:38:12 +0200

    feat: highlighted git file rendering

--- lua/configs/ntree.lua
+++ lua/configs/ntree.lua
@@ -7,9 +7,26 @@ require("nvim-tree").setup({
     },
     filters = {
         dotfiles = false,
+        git_ignored = false,
     },
     git = {
         enable = true,
         ignore = false,
+        show_on_dirs = true,
+    },
+    renderer = {
+        highlight_git = true,
+        icons = {
+            show = {
+                git = false,
+            },
+            glyphs = {
+                git = {
+                    ignored = "",
+                },
+            },
+        },
     },
 })
+
+vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", { fg = "#888888", bold = false, italic = true })
