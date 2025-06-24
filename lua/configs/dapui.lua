730cf5efbfe9cac7bd637dcc0277b962479cbf48
Brian Ferri <me@brianferri.dev>
2025-06-24 21:02:41 +0200

    feat (wip): dap picking

--- lua/configs/dapui.lua
+++ lua/configs/dapui.lua
@@ -1,4 +1,25 @@
 require("dapui").setup()
+local dap = require("dap")
+
+dap.adapters.lldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+        command = "codelldb",
+        args = { "--port", "${port}" },
+    },
+}
 
 -- Custom commands
-require("plugins.dap.run").setup({})
+require("plugins.dap.run").setup({
+    adapter_options = {
+        codelldb = {
+            request = "launch",
+            adapter = dap.adapters.lldb,
+            config = {
+                stopOnEntry = false,
+                args = {},
+            },
+        },
+    },
+})
--- lua/plugins/dap/run.lua
+++ lua/plugins/dap/run.lua
@@ -7,26 +7,37 @@ local previewers = require("telescope.previewers")
 local preview_utils = require("telescope.previewers.utils")
 local conf = require("telescope.config").values
 
+local dap = require("dap")
 local Path = require("plenary.path")
 local scan = require("plenary.scandir")
 
 local M = {}
 
+--- @class DapAdapterDefinition
+--- @field name string Name of the registered adapter (e.g. "lldb")
+--- @field request string (e.g. "launch" or "attach")
+--- @field config table Additional launch config fields (like args, stopOnEntry, etc.)
+--- @field adapter dap.Adapter
+---
 --- @class DapRunConfig
 --- @field search_root string? Directory to search executables from.
+--- @field adapter_options table<string, DapAdapterDefinition>
 
 --- @type DapRunConfig
 local user_config = {
     search_root = ".",
+    adapter_options = {},
 }
 
---- Apply user configuration
+--- Apply user configuration and register DAP adapters
 --- @param opts DapRunConfig
 function M.configure(opts)
-    opts = opts or {}
-    if opts.search_root and opts.search_root ~= "" then
-        user_config.search_root = opts.search_root
-    end
+    user_config.search_root = opts.search_root or user_config.search_root
+    user_config.adapter_options = vim.tbl_extend(
+        "force",
+        user_config.adapter_options,
+        opts.adapter_options
+    )
 end
 
 --- Scan for executable files under search_root
@@ -46,18 +57,34 @@ local function find_executables()
     return results
 end
 
---- Start dap session with selected binary
+--- Pick a DAP adapter
+--- @param callback fun(cfg: DapAdapterDefinition)
+local function pick_adapter(callback)
+    local opts = user_config.adapter_options
+    local keys = vim.tbl_keys(opts)
+    if #keys == 0 then
+        vim.notify("No DAP adapter options configured.", vim.log.levels.WARN)
+        return
+    end
+    if #keys == 1 then
+        callback(opts[keys[1]])
+        return
+    end
+    vim.ui.select(keys, { prompt = "Select DAP Adapter:" }, function(choice)
+        if choice then callback(opts[choice]) end
+    end)
+end
+
+--- Run nvim-dap with selected adapter and executable
 --- @param path string
-local function run_dap(path)
-    local dap = require("dap")
-    local cwd = Path:new(path):parent():absolute()
-    dap.run({
-        name = "Run Executable",
-        type = "lldb",
-        request = "launch",
-        program = path,
-        cwd = cwd,
-    })
+--- @param cfg DapAdapterDefinition
+local function run_dap(path, cfg)
+    dap.run(
+        vim.tbl_deep_extend("force", cfg.config, {
+            program = path,
+            cwd = Path:new(path):parent():absolute(),
+        })
+    )
 end
 
 --- Creates entry maker for executable files
@@ -67,15 +94,12 @@ local function create_entry_maker(displayer)
     local devicons = require("nvim-web-devicons")
     return function(path)
         local filename = Path:new(path):make_relative()
-        local icon, icon_hl = devicons.get_icon(filename, nil, { default = true })
+        local icon, hl = devicons.get_icon(filename, nil, { default = true })
         return {
             value = path,
             ordinal = filename,
             display = function()
-                return displayer({
-                    { icon, icon_hl },
-                    filename,
-                })
+                return displayer({ { icon, hl }, filename })
             end,
             path = path,
         }
@@ -85,22 +109,20 @@ end
 --- Previewer: show file info in a buffer
 local previewer = previewers.new_buffer_previewer({
     define_preview = function(self, entry)
-        local path = entry.path
-        local stat = vim.loop.fs_stat(path)
-        local lines = {
-            "Path: " .. path,
+        local stat = vim.loop.fs_stat(entry.path)
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {
+            "Path: " .. entry.path,
             "Size: " .. (stat and stat.size or "unknown") .. " bytes",
             "Modified: " .. (stat and os.date("%c", stat.mtime.sec) or "unknown"),
-        }
-        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+        })
         preview_utils.highlighter(self.state.bufnr, "markdown")
     end,
 })
 
 --- Launch telescope picker to select executable
 function M.dap_run()
-    local results = find_executables()
-    if vim.tbl_isempty(results) then
+    local executables = find_executables()
+    if vim.tbl_isempty(executables) then
         vim.notify("No executables found under: " .. user_config.search_root, vim.log.levels.WARN)
         return
     end
@@ -116,7 +138,7 @@ function M.dap_run()
     pickers.new({}, {
         prompt_title = "Run Executable (DAP)",
         finder = finders.new_table({
-            results = results,
+            results = executables,
             entry_maker = create_entry_maker(displayer),
         }),
         previewer = previewer,
@@ -126,7 +148,9 @@ function M.dap_run()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry()
                 if selection then
-                    run_dap(selection.path)
+                    pick_adapter(function(adapter_cfg)
+                        run_dap(selection.path, adapter_cfg)
+                    end)
                 end
             end)
             return true
