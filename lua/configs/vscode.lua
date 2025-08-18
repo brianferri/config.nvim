226dac723f2020a36d53876612ba55b24440b5e9
Brian Ferri <me@brianferri.dev>
2025-08-18 12:27:25 +0200

    chore: change trail character

--- lua/configs/vscode.lua
+++ lua/configs/vscode.lua
@@ -8,3 +8,13 @@ require('vscode').setup({
 })
 
 vim.cmd.colorscheme "vscode"
+
+-- https://www.manjotbal.ca/blog/neovim-whitespace.html
+vim.api.nvim_set_hl(0, 'TrailingWhitespace', { bg = 'Red', fg = 'Red' })
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*',
+    command = [[
+    syntax clear TrailingWhitespace |
+    syntax match TrailingWhitespace "\_s\+$"
+    ]]
+})
