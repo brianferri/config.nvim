3d91edd3faebe4f9d1ffbce83b21fcced1126d7d
Brian Ferri <me@brianferri.dev>
2025-08-20 09:28:49 +0200

    chore: swap bg and fg color; formatting

--- lua/configs/vscode-theme.lua
+++ lua/configs/vscode-theme.lua
@@ -10,11 +10,11 @@ require('vscode').setup({
 vim.cmd.colorscheme "vscode"
 
 -- https://www.manjotbal.ca/blog/neovim-whitespace.html
-vim.api.nvim_set_hl(0, 'TrailingWhitespace', { fg = 'Red' })
+vim.api.nvim_set_hl(0, 'TrailingWhitespace', { bg = 'Red' })
 vim.api.nvim_create_autocmd('BufEnter', {
     pattern = '*',
     command = [[
-    syntax clear TrailingWhitespace |
-    syntax match TrailingWhitespace "\_s\+$"
+        syntax clear TrailingWhitespace |
+        syntax match TrailingWhitespace "\_s\+$"
     ]]
 })
