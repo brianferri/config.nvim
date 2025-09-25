03b4e59d83c00a53f740b4bf3ffb37b97a5c2cf6
Brian Ferri <me@brianferri.dev>
2025-09-25 16:42:23 +0200

    fix: deletion keybinds, movement cursor placements

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -39,14 +39,7 @@ map(all_modes, '<M-q>', "<CMD>qa<CR>", "Quit All")
 map(all_modes, '<M-S-w>', "<CMD>q<CR>", "Close Window")
 map(all_modes, '<M-w>', "<CMD>bp <BAR> bd #<CR>", "Close Tab")
 map(all_modes, '<M-s>', "<CMD>w<CR>", "Write File")
-map(all_modes, '<M-f>', "<ESC>/", "Search In File")
 map(all_modes, '<M-S-f>', "<CMD>Telescope live_grep<CR>", "Search All Files")
-
--- Movement
-map({ 'i', 'n' }, '<M-Right>', "<ESC>lwi", "Move To Next Word")
-map({ 'i', 'n' }, '<M-Left>', "<ESC>bi", "Move To Previous Word")
-map('v', '<M-S-Right>', "lw", "Shift Selection Right By A Word")
-map('v', '<M-S-Left>', "b", "Shift Selection Left By A Word")
 
 -- Select All, Move Lines, Selection Manipulation
 -- TODO add comment on selection/line
@@ -62,15 +55,20 @@ map({ 'i', 'n' }, '<M-Down>', '<CMD>m .+1<CR>', "Move Line Down")
 map('v', '<M-Up>', ":m '<-2<CR>gv=gv", "Move Selection Up")
 map('v', '<M-Down>', ":m '>+1<CR>gv=gv", "Move Selection Down")
 
-map({ 'n', 'v' }, '<M-BS>', 'vb"_d', "Delete Word Before Cursor")
-map({ 'n', 'v' }, '<M-Del>', 've"_d', "Delete Word After Cursor")
-map('i', '<M-BS>', '<ESC>vb"_di', "Delete Word Before Cursor")
-map('i', '<M-Del>', '<Right><ESC>ve"_di', "Delete Word After Cursor")
+map({ 'i', 'n' }, '<M-Right>', "<ESC>ea", "Move To Next Word")
+map({ 'i', 'n' }, '<M-Left>', "<ESC>bi", "Move To Previous Word")
+map('v', '<M-S-Right>', "e", "Shift Selection Right By A Word")
+map('v', '<M-S-Left>', "b", "Shift Selection Left By A Word")
+
+map({ 'n', 'v' }, '<M-BS>', 'hvbd', "Delete Word Before Cursor")
+map({ 'n', 'v' }, '<M-Del>', 'ved', "Delete Word After Cursor")
+map('i', '<M-BS>', '<C-w>', "Delete Word Before Cursor")
+map('i', '<M-Del>', '<Right><ESC>dei', "Delete Word After Cursor")
 
 map('v', '<Tab>', ">gv", "Indent Selection")
 map('v', '<S-Tab>', "<gv", "Unindent Selection")
-map('v', '<BS>', '"_d', "Delete Selection")
-map('v', '<DEL>', '"_d', "Delete Selection")
+map('v', '<BS>', 'd', "Delete Selection")
+map('v', '<DEL>', 'd', "Delete Selection")
 
 map('v', '<', "<Plug>(nvim-surround-visual)>", "Surround Selection With `<>`")
 map('v', '(', "<Plug>(nvim-surround-visual))", "Surround Selection With `()`")
