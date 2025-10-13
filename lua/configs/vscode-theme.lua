9efd595df91f8807fce0691f0dfb4d8b503e32f7
Brian Ferri <me@brianferri.dev>
2025-10-13 12:19:04 +0200

    feat: Better the better comments

--- lua/configs/vscode-theme.lua
+++ lua/configs/vscode-theme.lua
@@ -27,6 +27,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
 -- Better Comments
 -----------------------------------------------------------
 
+vim.hl.priorities.semantic_tokens = 75
+
 local ns = vim.api.nvim_create_namespace("BetterComments")
 
 --- @alias BetterCommentsConfig table<string, vim.api.keyset.highlight>
@@ -44,7 +46,7 @@ local comment_hls = {
 --- @param marker string
 --- @return string
 local function hl_group_name(marker)
-    return "BetterComment_" .. marker:gsub("%W", function(c)
+    return "BetterComment" .. marker:gsub("%W", function(c)
         return string.byte(c)
     end)
 end
@@ -59,7 +61,7 @@ end
 --- @param bufnr integer
 local function highlight_comments(bufnr)
     local lang = vim.bo[bufnr].filetype
-    local ok, query = pcall(vim.treesitter.query.parse, lang, "(comment) @c")
+    local ok, comments = pcall(vim.treesitter.query.parse, lang, "(comment) @comment")
     if not ok then return end
 
     local parser = vim.treesitter.get_parser(bufnr, lang, {})
@@ -68,22 +70,26 @@ local function highlight_comments(bufnr)
     local root = parser:parse()[1]:root()
     vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
 
-    for _, node in query:iter_captures(root, bufnr, 0, -1) do
+    for _, node in comments:iter_captures(root, bufnr, 0, -1) do
+        local linenr, col = node:range()
         local text = vim.treesitter.get_node_text(node, bufnr)
         local i = 0
         -- ? We want to handle each line separately, allowing us to encode
         -- ? meaning, potentially, in different lines of a multiline comment
         for line in text:gmatch("[^\n]+") do
             if #line > 1024 then goto continue end
-            for pat, _ in pairs(comment_hls) do
-                if line:match(pat) then
-                    local linenr, col, elinenr, ecol = node:range()
-                    vim.api.nvim_buf_set_extmark(bufnr, ns, linenr + i, col, {
-                        end_col = ecol,
-                        end_line = elinenr,
-                        hl_group = hl_group_name(pat),
+            for pattern, _ in pairs(comment_hls) do
+                for padding, hl_comment_text in line:gmatch("(.*)(" .. pattern .. "[^\n]+)$") do
+                    local start_col = (i > 0) and #padding or col
+                    local end_col = start_col + ((i > 0) and #hl_comment_text or #line)
+                    vim.api.nvim_buf_set_extmark(bufnr, ns, linenr + i, start_col, {
+                        end_col = end_col,
+                        hl_group = hl_group_name(pattern),
+                        -- ! We don't want to override all treesitter highlighting (default 100) just the `@comment`
+                        -- TODO: Lower the `@comment` priority to 98
+                        priority = 99,
                     })
-                    break
+                    goto continue
                 end
             end
             ::continue::
@@ -92,8 +98,8 @@ local function highlight_comments(bufnr)
     end
 end
 
-local group = vim.api.nvim_create_augroup("BetterComments", { clear = true })
 set_hl_groups(comment_hls)
+local group = vim.api.nvim_create_augroup("BetterComments", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI" }, {
     group = group,
     callback = function(args) highlight_comments(args.buf) end,
