9c2b63377adbeaa59640cd38022f6c15c7c2b838
Brian Ferri <me@brianferri.dev>
2025-10-30 19:10:41 +0100

    fix: don't show trailing whitespaces in non-file buffers

--- lua/plugins/vscode-theme/trailing_whitespaces.lua
+++ lua/plugins/vscode-theme/trailing_whitespaces.lua
@@ -1,11 +1,12 @@
 -- https://www.manjotbal.ca/blog/neovim-whitespace.html
 vim.api.nvim_set_hl(0, 'TrailingWhitespace', { bg = 'Red' })
-vim.api.nvim_create_autocmd('BufEnter', {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'TermOpen' }, {
     pattern = '*',
-    command = [[
-        syntax clear TrailingWhitespace |
-        syntax match TrailingWhitespace "\_s\+$"
-    ]]
+    callback = function(args)
+        vim.cmd([[ syntax clear TrailingWhitespace ]])
+        if vim.bo[args.buf].buftype ~= '' then return end
+        vim.cmd([[ syntax match TrailingWhitespace "\_s\+$" ]])
+    end,
 })
 vim.api.nvim_create_user_command("RemoveTrailing",
     [[ silent keeppatterns %s/\s\+$//e ]],
