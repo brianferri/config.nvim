f2d662fd4c9b64f3d39862310d9fd6b0e0e037d6
Brian Ferri <me@brianferri.dev>
2026-02-11 15:11:56 +0100

    feat: apply rainbow delimiters to punctuation

--- lua/configs/ibl.lua
+++ lua/configs/ibl.lua
@@ -1,10 +1,4 @@
-local highlight = {
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowViolet",
-}
 local hooks = require "ibl.hooks"
-
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -15,7 +9,25 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
+local highlight = {
+    "RainbowYellow",
+    "RainbowViolet",
+    "RainbowBlue",
+}
 
+vim.g.rainbow_delimiters = {
+    highlight = highlight,
+    strategy = {
+        [''] = 'rainbow-delimiters.strategy.global',
+        vim = 'rainbow-delimiters.strategy.local',
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+    },
+    priority = {
+        [''] = 110,
+    },
+}
+
+require("ibl").setup { scope = { highlight = highlight } }
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
