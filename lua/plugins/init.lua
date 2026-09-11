81cd94a10b6ac53eb7f607fdbcddd9dc1660c269
Brian Ferri <me@brianferri.dev>
2026-09-11 13:55:35 +0200

    chore: set events on lazy loading

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,28 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
+        -- ? `configs.telescope` is what defines `SearchReplace` and `OpenRecent`,
+        -- ? so both have to be listed here for the stubs to reach it
+        cmd = { "Telescope", "SearchReplace", "OpenRecent" },
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function() require "configs.telescope" end
     },
     {
         "3rd/image.nvim",
         build = false,
+        event = "VeryLazy",
         opts = { processor = "magick_cli" },
         dependencies = { "nvim-telescope/telescope.nvim" }
     },
     {
         "akinsho/git-conflict.nvim",
         version = "*",
+        event = "VeryLazy",
         config = true
     },
     {
         'MeanderingProgrammer/render-markdown.nvim',
+        ft = "markdown",
         dependencies = {
             'nvim-tree/nvim-web-devicons',
             'nvim-treesitter/nvim-treesitter',
@@ -26,17 +32,22 @@ return {
     {
         "akinsho/bufferline.nvim",
         version = "*",
+        event = "VeryLazy",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function() require "configs.bufferline" end
     },
     {
         "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function() require "configs.lualine" end
     },
     {
+        -- ! Making it eager since its config is what clears `netrw`,
+        -- ! and a lazy load would let netrw claim the first directory opened
         "nvim-tree/nvim-tree.lua",
         version = "*",
+        lazy = false,
         priority = 999,
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function() require "configs.nvimtree" end,
@@ -50,6 +61,8 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "Mason", "MasonLog", "LspInfo" },
         dependencies = {
             { "williamboman/mason.nvim",           config = true },
             { "williamboman/mason-lspconfig.nvim", config = true },
@@ -63,6 +76,7 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
+        event = { "InsertEnter", "CmdlineEnter" },
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
@@ -74,10 +88,12 @@ return {
         config = function() require "configs.cmp" end
     },
     {
-        "voldikss/vim-floaterm"
+        "voldikss/vim-floaterm",
+        cmd = { "FloatermNew", "FloatermToggle", "FloatermNext", "FloatermPrev" },
     },
     {
         "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         config = function() require "configs.gitsigns" end
     },
     {
@@ -96,7 +112,8 @@ return {
         config = function() require "configs.dap" end
     },
     {
-        "RRethy/vim-illuminate"
+        "RRethy/vim-illuminate",
+        event = { "BufReadPost", "BufNewFile" },
     },
     {
         "kylechui/nvim-surround",
@@ -111,11 +128,13 @@ return {
     },
     {
         "norcalli/nvim-colorizer.lua",
+        event = { "BufReadPost", "BufNewFile" },
         config = function() require "colorizer".setup() end,
     },
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
+        event = { "BufReadPost", "BufNewFile" },
         dependencies = { {
             "HiPhish/rainbow-delimiters.nvim",
             submodules = false,
@@ -124,7 +143,7 @@ return {
     },
     {
         "linux-cultist/venv-selector.nvim",
-        lazy = false,
+        ft = "python",
         opts = {},
     },
 }
